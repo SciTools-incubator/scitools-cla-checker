94c6a5da5a3073ac65d6310f684ad84fe58a090d
Phil Elson <pelson.pub@gmail.com>
2017-08-11 09:47:51 +0100

    Added a commit so that I can test a PR.

--- scitools_cla_checker/update_pr.py
+++ scitools_cla_checker/update_pr.py
@@ -1,6 +1,7 @@
 import json
 import os
 from pprint import pprint
+from pathlib import Path
 import sys
 
 import requests
@@ -9,23 +10,35 @@ import tornado.httpclient
 from tornado.ioloop import IOLoop
 
 
+def api_token():
+    token_file = Path('token.txt')
+    if not token_file.exists():
+        raise IOError('Please create a token at github.com, and save it in {}'.format(token_file))
+
+    token = token_file.read_text().strip()
+    return token
+
+
 CONTRIBUTORS_DOC = 'https://raw.githubusercontent.com/SciTools/scitools.org.uk/gh-pages/contributors.json'
 
 
+token = api_token()
+
+
 @gen.coroutine
-def get_contributors():
+def update_pr_no_cla():
     http_client = tornado.httpclient.AsyncHTTPClient()
-    response = yield http_client.fetch(CONTRIBUTORS_DOC)
-    if response.error:                                                   
-        raise ValueError('Unable to fetch contributors.json')
-    content = json.loads(response.body)
+    headers = {'Authorization': 'token {}'.format(token),
+               'Accept': 'application/vnd.github.v3+json'}
+    URL = 'https://api.github.com/repos/pelson/scitools-cla-checker/issues/1/labels'
+    content = ['testing']
+    response = yield http_client.fetch(URL, body=json.dumps(content).encode(), method='POST',
+                                       headers=headers)
+    if response.error:
+        raise RuntimeError(response.body)
+    content = json.loads(response.body.decode())
+    print(content)
     return content
-
-
-#def get_contributors_doc():
-#    resp = requests.get('https://raw.githubusercontent.com/SciTools/scitools.org.uk/gh-pages/contributors.json')
-#    content = resp.json()
-#    print(content)
 
 
 def configure_default_client():
@@ -48,17 +61,8 @@ def main():
                         help='If defined, check that the given name is in the contributors list')
     args = parser.parse_args()
     configure_default_client()
-    contributors = IOLoop.current().run_sync(get_contributors)
-    signatures = sorted([person['profile_name'] for person in contributors['contributors']])
-    if args.check is None:
-        print('\n'.join(signatures))
-    else:
-        if args.check not in signatures:
-            print('{} is not in the list of contributors ({})'.format(args.check, ', '.join(signatures)))
-            sys.exit(1)
-        else:
-            print('{} is in the list of contributors'.format(args.check))
-            sys.exit(0)
+    contributors = IOLoop.current().run_sync(update_pr_no_cla)
+
 
 
 if __name__ == '__main__':
