4f31e18eb63846ad4c3316fa61a77f6f4d2d3402
Phil Elson <pelson.pub@gmail.com>
2017-08-12 07:48:21 +0100

    Removed all flake.

--- scitools_cla_checker/__main__.py
+++ scitools_cla_checker/__main__.py
@@ -1,10 +1,5 @@
-import json
-import os
-from pprint import pprint
 import sys
 
-from tornado import gen
-import tornado.httpclient
 from tornado.ioloop import IOLoop
 
 from . import update_pr
@@ -12,9 +7,12 @@ from . import update_pr
 
 def main():
     import argparse
-    parser = argparse.ArgumentParser(description='Print/check the contributors list')
-    parser.add_argument('check', default=None, nargs='?',
-                        help='If defined, check that the given name is in the contributors list')
+    parser = argparse.ArgumentParser(
+            description='Print/check the contributors list')
+    parser.add_argument(
+            'check', default=None, nargs='?',
+            help=('[If defined] Check that the given name is in the '
+                  'contributors list'))
     args = parser.parse_args()
     update_pr.configure_default_client()
     signatures = IOLoop.current().run_sync(update_pr.get_contributors)
@@ -22,7 +20,8 @@ def main():
         print('\n'.join(signatures))
     else:
         if args.check not in signatures:
-            print('{} is not in the list of contributors ({})'.format(args.check, ', '.join(signatures)))
+            print('{} is not in the list of contributors ({})'
+                  ''.format(args.check, ', '.join(signatures)))
             sys.exit(1)
         else:
             print('{} is in the list of contributors'.format(args.check))
